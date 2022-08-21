d3ff0472aff419050fb725e7222c863787c47686
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-21 12:42:05 +0100

    updates: conditional statement method

--- playground.dart
+++ playground.dart
@@ -1,21 +1,6 @@
-import "dart:io";
-
-class Num {
-  int num = 10;
-}
-
-main() {
-  //null-aware operation(?.) (??) (??=)
-  var n;
-  int number;
-  number = n?.num ?? 0;
-  print(number);
-
-  var m = Num();
-  int value;
-  value = m.num;
-  print(value);
-
-  int? defaultNullText;
-  print(defaultNullText ??= 100);
+void main() {
+  double x = 100.1;
+  if (x is double) {
+    print('x is $x');
+  }
 }
