89b2495304f6a2a2ca6fc5ee97c048840bb436e0
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-21 14:39:33 +0100

    updates: list, set and map

--- playground.dart
+++ playground.dart
@@ -1,6 +1,38 @@
 void main() {
-  double x = 100.1;
-  if (x is double) {
-    print('x is $x');
+  //List - Array in js
+  List<String> names = ['muba', 'shir'];
+  print(names[0]);
+
+  //set - unique unordered list
+
+  Set halogens = {'fluorine', 'chlorine', 'chlorine'};
+
+  for (var x in halogens) {
+    print(x);
   }
+  //for empty set
+  var emptySet = <String>{};
+  //or
+  Set<String> emptySet2 = {};
+
+  print(emptySet.runtimeType);
+  print(emptySet2.runtimeType);
+
+  //Maps - object in js
+
+  Map employee = {
+    "name": 'mubashir',
+    "lastName": 'Asiyanbi',
+    "phone": '0918281212'
+  };
+  print(employee['name']);
+
+  var technicians = Map();
+
+  technicians['mubashir'] = {
+    'id': '121921217621',
+    'fullName': 'Asiyanbi Mubashir'
+  };
+
+  print(technicians);
 }
