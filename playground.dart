728e76c2dc92719f906e81d72ba8094a6297c171
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-21 01:08:34 +0100

    feat: type conversion and assert

--- playground.dart
+++ playground.dart
@@ -1,7 +1,25 @@
 import "dart:io";
 
 main() {
+  //input and output
   stdout.writeln('What is your Name?');
   String? fullname = stdin.readLineSync();
   print('my name is $fullname');
+
+//boolean type
+  bool isTrusted = true;
+  var canTrust = false;
+  print(isTrusted);
+
+  // dynamic type
+  dynamic weakVariable = 100;
+  print('before : $weakVariable');
+  weakVariable = 'this is a string';
+  print('after: $weakVariable');
+
+  //type conversion
+
+  var oneConvertion = int.parse('90');
+  print(oneConvertion);
+  assert(oneConvertion == 90);
 }
