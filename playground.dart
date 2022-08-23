70eefec004b07aab9e40029cf5941e950bf3c19e
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-23 02:22:23 +0100

    update: default class constructoe method 1

--- playground.dart
+++ playground.dart
@@ -1,15 +1,22 @@
-void main() {
-  Person person1 = Person();
-  person1.name = 'jagunlabi';
-  print(person1.name);
-}
-
 class Person {
   String? name;
   int? age;
+
+  //default constructor
+  Person(String name, [int age = 10]) {
+    this.name = name;
+    this.age = age;
+  }
 
   void showOutput() {
     print(age);
     print(name);
   }
+}
+
+void main() {
+  Person person1 = Person('jagunlabi', 20);
+  person1.name = 'jagunlabi';
+  print(person1.name);
+  print(person1.age);
 }
