b1a9ba84be375e7339463800b0206bc8dacb4f1c
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-23 02:19:50 +0100

    update: class

--- playground.dart
+++ playground.dart
@@ -1,17 +1,15 @@
 void main() {
-  //positional function
-  print(sum(10, 12));
-
-  //positional function with optional params
-  print(sum(10));
-
-  //un-positional function
-  print(sum2(num1: 10, num2: 20));
-
-  //un-positional function with default params
-  print(sum2(num1: 10));
+  Person person1 = Person();
+  person1.name = 'jagunlabi';
+  print(person1.name);
 }
 
-dynamic sum(var num1, [var num2]) => num1 + (num2 ?? 0);
+class Person {
+  String? name;
+  int? age;
 
-dynamic sum2({var num1, var num2 = 10}) => num1 + num2;
+  void showOutput() {
+    print(age);
+    print(name);
+  }
+}
