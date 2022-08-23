6d56a7acdded6ae3398955a9374c94a589b83601
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-23 02:35:58 +0100

    update: class named constructor

--- playground.dart
+++ playground.dart
@@ -3,20 +3,24 @@ class Person {
   int? age;
 
   //default constructor
-  Person(String name, [int age = 10]) {
-    this.name = name;
-    this.age = age;
+  Person(this.name, [this.age = 10]);
+
+  //named constructor
+  Person.guest() {
+    name = 'Asiyanbi';
+    age = 40;
   }
 
   void showOutput() {
-    print(age);
     print(name);
+    print(age);
   }
 }
 
 void main() {
   Person person1 = Person('jagunlabi', 20);
-  person1.name = 'jagunlabi';
-  print(person1.name);
-  print(person1.age);
+  person1.showOutput();
+
+  var person2 = Person.guest();
+  person2.showOutput();
 }
