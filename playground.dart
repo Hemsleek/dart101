77fb9d068b5e0012c38ef867494a998b59b25ed1
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-09-01 02:07:07 +0100

    update: Generics

--- playground.dart
+++ playground.dart
@@ -1,27 +1,12 @@
-//iterables
+//Generics
+class Pair<A, B> {
+  final A value1;
+  final B value2;
 
-//sync iterable
-Iterable<int> getOneTwoThree() sync* {
-  yield 1;
-  yield 2;
-  yield 3;
+  Pair(this.value1, this.value2);
 }
 
-//async iterable
-Stream<Iterable<int>> getAsyncOneTwoThree() async* {
-  yield [1, 2, 3];
-  yield [4, 5, 6];
-  yield [17, 8, 9];
-}
-
-main() async {
-  print(getOneTwoThree());
-
-  for (final value in getOneTwoThree()) {
-    print(value);
-  }
-
-  await for (final value in getAsyncOneTwoThree()) {
-    print(value);
-  }
+main() {
+  final name = Pair('hi', 2);
+  print(name.value2);
 }
